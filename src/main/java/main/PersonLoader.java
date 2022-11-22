ec5f1602982254ef02946ffb7efdb2bdc65e7ceb
Maryia <simonna13@bk.ru>
2022-11-22 22:20:18 +0300

    Loader fix

--- src/main/java/main/PersonLoader.java
+++ src/main/java/main/PersonLoader.java
@@ -10,7 +10,7 @@ public class PersonLoader {
 		
 		Person person = new Person(null, 35, "Yuri", "Slabko");
 		EntityManager em = HybernateUtil.getEntityManager();
-		em.getTransaction();
+		em.getTransaction().begin();
 		em.persist(person);
 		em.getTransaction().commit();
 		HybernateUtil.close();
